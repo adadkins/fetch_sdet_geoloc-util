18c83553375a4e6a4031e837c40901dcadabb0a8
Anthony Adkins <anthonyadkins@MacBookPro.lan>
2025-03-19 18:48:08 -0700

    fix: clean up test name

--- app_test.go
+++ app_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAmazonTrucks(t *testing.T) {
+func TestSearchLocationNameOrPostCode(t *testing.T) {
 	t.Run("Can search for a Post code and City name", func(t *testing.T) {
 		//given
 		expectedResults := []types.SearchResult{
